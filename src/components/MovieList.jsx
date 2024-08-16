7171d629365f299cba6f77fdb854c6b53471dee5
Rei Ramadani <ramadanirei@gmail.com>
2024-08-16 13:42:07 +0200

    The creation of the movie app

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 
 const MovieList = (props) => {
+  const FavouriteComponent = props.favouriteComponent;
+
   return (
-    <>
+    <div style={{ display: "flex", gap: "2rem" }}>
       {props.movies.map((movie, index) => (
-        <div className="image-container d-flex justify-content-start m3">
-          <img src={movie.Poster} alt="movie poster" />
+        <div className="image-container d-flex justify-content-start m-12">
+          <img
+            src={movie.Poster}
+            style={{ width: "300px", height: "500px" }}
+            alt="movie"
+          ></img>
+          <div
+            onClick={() => props.handleFavouritesClick(movie)}
+            className="overlay d-flex align-items-center justify-content-center"
+          >
+            <FavouriteComponent />
+          </div>
         </div>
       ))}
-    </>
+    </div>
   );
 };
 
